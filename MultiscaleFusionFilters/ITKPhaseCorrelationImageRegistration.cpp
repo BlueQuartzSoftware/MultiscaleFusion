1a6d24fc16b8cdfb5ceb7f225b2ff6179f641dee
Joey Kleingers <joey.kleingers@bluequartz.net>
2019-03-26 11:53:51 -0400

    Updating connections and other APIs to be consistent with the new messaging design.

--- MultiscaleFusionFilters/ITKPhaseCorrelationImageRegistration.cpp
+++ MultiscaleFusionFilters/ITKPhaseCorrelationImageRegistration.cpp
@@ -163,9 +163,8 @@ void ITKPhaseCorrelationImageRegistration::filter()
     notifyWarningMessage(getHumanLabel(), transformParameters, 0);
   } catch(itk::ExceptionObject& err)
   {
-    setErrorCondition(-55558);
     QString errorMessage = "ITK exception was thrown while filtering input image: %1";
-    notifyErrorMessage(getHumanLabel(), errorMessage.arg(err.GetDescription()), getErrorCondition());
+    notifyErrorMessage("", errorMessage.arg(err.GetDescription()), -55558);
     return;
   }
 }
